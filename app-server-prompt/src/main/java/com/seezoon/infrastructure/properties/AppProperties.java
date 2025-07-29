ab5ff6d245d40a924a780604e9978712c9371786
dfenghuang <dfenghuang@tencent.com>
2025-07-29 10:55:26 +0800

    prompt

--- app-server-prompt/src/main/java/com/seezoon/infrastructure/properties/AppProperties.java
+++ app-server-prompt/src/main/java/com/seezoon/infrastructure/properties/AppProperties.java
@@ -29,16 +29,6 @@ public class AppProperties {
     @NotNull
     @NestedConfigurationProperty
     private CorsProperties cors = new CorsProperties();
-
-    @Valid
-    @NotNull
-    @NestedConfigurationProperty
-    private WxProperties wx = new WxProperties();
-
-    @Valid
-    @NotNull
-    @NestedConfigurationProperty
-    private AliyunProperties aliyun = new AliyunProperties();
     /**
      * 文件、图片前缀
      */
